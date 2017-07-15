301320613fe09717699be4df6ab544c791a21dc8
Andogar <robert.andoga@gmail.com>
2017-07-15 15:51:32 -0400

    Attempting to fix heroku host

--- program.js
+++ program.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const application = require('./application.js');
 
-application.listen(3000);
+application.listen(process.env.PORT, '0.0.0.0', function(error) {
+  console.log("Started listening on %s", application.url);
+});
